72c6331e0fd71fd33234a73aaae90716d869bdad
lelopezp <161893492+lelopezp@users.noreply.github.com>
2025-04-29 21:34:17 -0700

    yea this prolly isnt it either for printing

--- crawler/worker.py
+++ crawler/worker.py
@@ -32,3 +32,6 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+        print("final result call hopefully")
+        scraper.result()
+        print("end of program, should be")
